72727218a5074bfe0bc4fdf36815df85b6dd86f2
Sylkaron <pabloberton@sfr.fr>
2022-02-11 16:32:19 +0100

    principal

--- classe_principale.py
+++ classe_principale.py
@@ -1,1 +1,13 @@
+
+import numpy as np
+import sklearn as sk
+import scipy as sp
+
+import pandas as pd
+import matplotlib.pyplot as plt
+
+dataBirdPresence = pd.read_csv('/src/ff1010bird_metadata.csv')
+
 #ceci_est_un_test
+
+#le test est validé
