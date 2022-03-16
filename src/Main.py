9c66613c2e27e680d58982fc877cfdd42a76b597
Sylkaron <92585017+Sylkaron@users.noreply.github.com>
2022-03-16 10:06:05 +0100

    reseau keras

--- src/Main.py
+++ src/Main.py
@@ -7,4 +7,28 @@ import scipy as sp
 import pandas as pd
 import matplotlib.pyplot as plt
 
-dataBirdPresence = pd.read_csv('../Ressouces/ff1010bird_metadata.csv')
+import tensorflow as tf
+
+
+dataBirdPresence = pd.read_csv('../Ressouces/ff1010bird_metadata.csv', index_col = 'itemid')
+
+def reseauKeras(data):
+    global neuron
+    global optimiseur
+
+    neuron = tf.keras.Sequential([
+    tf.keras.layers.Dense(64, activation = 'relu', input_shape = tailleEntree),
+    tf.keras.layers.Dropout(0.3, seed = 2),
+    tf.keras.layers.Dense(64, activation = 'swish'),
+    tf.keras.layers.Dense(64, activation = 'relu'),
+    tf.keras.layers.Dense(64, activation = 'swish'),
+    tf.keras.layers.Dense(64, activation = 'relu'),
+    tf.keras.layers.Dense(64, activation = 'swish'),
+    tf.keras.layers.Dense(1)])
+
+    optimiseur = tf.keras.optimizers.RMSprop(learning_rate=0.001)
+    neuron.compile(loss = tf.keras.losses.MeanSquaredError(),
+                   optimizer = optimiseur,
+                   metrics = ['mae'])
+
+    return
